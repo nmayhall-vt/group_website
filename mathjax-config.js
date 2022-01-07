48abd52b7259be325ad3b85394346a1cd8d9cdbc
Nick Mayhall <nmayhall@vt.edu>
2022-01-07 16:41:21 -0500

    added js file

---
+++ mathjax-config.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+	tex: {
+		inlineMath: [['$', '$'], ['\\(', '\\)']]
+	},
+	svg: {
+		fontCache: 'global'
+	},
+	loader: {load: ['[tex]/physics']},
+	Tex: {packages: {'[+]': ['physics']}}
+};
